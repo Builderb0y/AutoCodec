1d5c5cbfcff87460f61d0356dcebfb044c75ec30
Builderb0y <14227067+Builderb0y@users.noreply.github.com>
2025-05-14 00:15:14 -0500

    allow VersionedFixer to provide a default version when absent, fix VersionedFixer hard-coding the version key in appending stage.

--- src/main/java/builderb0y/autocodec/fixers/VersionedFixer.java
+++ src/main/java/builderb0y/autocodec/fixers/VersionedFixer.java
@@ -3,6 +3,7 @@ package builderb0y.autocodec.fixers;
 import org.jetbrains.annotations.ApiStatus.OverrideOnly;
 import org.jetbrains.annotations.NotNull;
 
+import builderb0y.autocodec.data.AbstractNumberData;
 import builderb0y.autocodec.fixers.AutoFixer.NamedFixer;
 import builderb0y.autocodec.reflection.reification.ReifiedType;
 
@@ -37,10 +38,15 @@ public abstract class VersionedFixer<T_Decoded> extends NamedFixer<T_Decoded> {
 
 	public abstract <T_Encoded> @NotNull DataFixContext<T_Encoded> fixData(@NotNull DataFixContext<T_Encoded> context, int version) throws DataFixException;
 
+	public <T_Encoded> int getDefaultVersion(@NotNull DataFixContext<T_Encoded> rootContext) throws DataFixException {
+		throw new DataFixException(() -> "Missing " + this.versionKey);
+	}
+
 	@Override
 	@OverrideOnly
 	public <T_Encoded> @NotNull DataFixContext<T_Encoded> fixData(@NotNull DataFixContext<T_Encoded> context) throws DataFixException {
-		int version = context.forceGetMember(this.versionKey).forceAsInt();
+		AbstractNumberData number = context.tryGetMember(this.versionKey).tryAsNumber();
+		int version = number != null ? number.intValue() : this.getDefaultVersion(context);
 		context = context.deepCopy();
 		context.removeMember(this.versionKey);
 		return this.fixData(context, version);
@@ -49,7 +55,6 @@ public abstract class VersionedFixer<T_Decoded> extends NamedFixer<T_Decoded> {
 	@Override
 	@OverrideOnly
 	public <T_Encoded> @NotNull DataAppendContext<T_Encoded, T_Decoded> appendData(@NotNull DataAppendContext<T_Encoded, T_Decoded> context) throws DataAppendException {
-		context.putInt("version", this.version);
-		return super.appendData(context);
+		return context.withInt(this.versionKey, this.version);
 	}
 }
